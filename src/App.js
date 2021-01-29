e52057b8aa68b1321e6d75b12fc67837202736ba
cabralh <hjcbr19@gmail.com>
2021-01-28 21:27:02 -0500

    updating components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 400,
+      density: {
+        enable: true,
+        value_area: 2000
+      }
+    }
+  }
+}
+// Next time let's make this interactive as well.
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className='particles'
+          params={particlesOptions}
+        />
         <Navigation />
         <Logo />
-        {/*<ImageLinkForm />
-        <FaceRecognition />} */}
+        <Rank />
+        <ImageLinkForm />
       </div>
     );
   }
